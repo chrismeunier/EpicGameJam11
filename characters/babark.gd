b7b66b0766ef210b62597cb2877e16f3b4baa9ca
Aurèle Jomini <aurele.jomini@regenhu.com>
2025-06-28 16:46:30 +0200

    merge sounds and visuals

--- characters/babark.gd
+++ characters/babark.gd
@@ -3,10 +3,6 @@ extends Node2D
 @onready var area_2d: Area2D = %Area2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = %AudioStreamPlayer2D
 
-func _ready() -> void:
-	area_2d.monitoring = true
-	area_2d.area_entered.connect(_on_area_entered)
-
-func _on_area_entered() -> void:
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if not audio_stream_player_2d.playing:
 		audio_stream_player_2d.play()
