c2fb9cc3efe49979aa384c88c68de252b5f79218
Hugo <Hugo.serdouragoncalves@gmail.com>
2025-06-28 13:40:12 +0200

    Miss understanding is now correctly animated

--- characters/player.gd
+++ characters/player.gd
@@ -2,22 +2,32 @@ extends Node2D
 
 var tilemap: TileMapLayer
 @onready var anim = $AnimatedSprite2D
+
 var lastDirection = Vector2.ZERO
+var can_move: bool = true
+const MOVE_COOLDOWN := 0.3  # seconds between moves
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Optional: preload or set up any timers
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("Down")):
-		move(Vector2.DOWN)
-	elif (Input.is_action_just_pressed("Up")):
-		move(Vector2.UP)
-	elif (Input.is_action_just_pressed("Right")):
-		move(Vector2.RIGHT)
-	elif (Input.is_action_just_pressed("Left")):
-		move(Vector2.LEFT)
+	if not can_move:
+		return
+		
+	var input_direction = Vector2.ZERO
+	if Input.is_action_just_pressed("Down"):
+		input_direction = Vector2.DOWN
+	elif Input.is_action_just_pressed("Up"):
+		input_direction = Vector2.UP
+	elif Input.is_action_just_pressed("Right"):
+		input_direction = Vector2.RIGHT
+	elif Input.is_action_just_pressed("Left"):
+		input_direction = Vector2.LEFT
+	
+	if input_direction != Vector2.ZERO:
+		move(input_direction)
 	else:
 		move(Vector2.ZERO)
 
@@ -25,33 +35,42 @@ func set_tilemap(tmap: TileMapLayer):
 	tilemap = tmap
 
 func move(direction: Vector2):
+	if direction == Vector2.ZERO:
+		handle_idle()
+		return
+	
+	can_move = false  # lock input
+	animate_player(direction)
+
 	var currentTile = tilemap.local_to_map(global_position)
 	var targetTile: Vector2i = Vector2i(
-		currentTile.x + direction.x,
-		currentTile.y + direction.y,
+		currentTile.x + int(direction.x),
+		currentTile.y + int(direction.y),
 	)
-	animate_player(direction)
+	
 	global_position = tilemap.map_to_local(targetTile)
 	lastDirection = direction
+	
+	# Start cooldown
+	await get_tree().create_timer(MOVE_COOLDOWN).timeout
+	can_move = true
 
 func animate_player(direction: Vector2) -> void:
-	if (direction == Vector2.DOWN):
+	if direction == Vector2.DOWN and anim.animation != "move_down":
 		anim.play("move_down")
-	elif (direction == Vector2.LEFT):
+	elif direction == Vector2.LEFT and anim.animation != "move_left":
 		anim.play("move_left")
-	elif (direction == Vector2.RIGHT):
+	elif direction == Vector2.RIGHT and anim.animation != "move_right":
 		anim.play("move_right")
-	elif (direction == Vector2.UP):
+	elif direction == Vector2.UP and anim.animation != "move_up":
 		anim.play("move_up")
-	elif (direction == Vector2.ZERO):
-		handle_idle()
 
 func handle_idle() -> void:
-	if (lastDirection == Vector2.DOWN):
+	if lastDirection == Vector2.DOWN and anim.animation != "idle_down":
 		anim.play("idle_down")
-	elif (lastDirection == Vector2.LEFT):
+	elif lastDirection == Vector2.LEFT and anim.animation != "idle_left":
 		anim.play("idle_left")
-	elif (lastDirection == Vector2.RIGHT):
+	elif lastDirection == Vector2.RIGHT and anim.animation != "idle_right":
 		anim.play("idle_right")
-	elif (lastDirection == Vector2.UP):
+	elif lastDirection == Vector2.UP and anim.animation != "idle_up":
 		anim.play("idle_up")
