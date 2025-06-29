e85fc7e2fc8638d0a3bcd49a636840ad0e7ff2e3
Christophe MULLER <christophe.muller@espi-suisse.ch>
2025-06-29 16:38:28 +0200

    testing canvas item for game levels

--- main_scene.gd
+++ main_scene.gd
@@ -1,26 +1,20 @@
 extends Node2D
 
-@onready var current_scene = $MarkerLvl1
-@onready var controls: ControlPanel = %Controls
-
-const CENTERED_LVL_1 = preload("res://CenteredLevels/centered_lvl_1.tscn")
-const CENTERED_LVL_2 = preload("res://CenteredLevels/centered_lvl_2.tscn")
-const CENTERED_LVL_3 = preload("res://CenteredLevels/centered_lvl_3.tscn")
-const CENTERED_LVL_4 = preload("res://CenteredLevels/centered_lvl_4.tscn")
-const CENTERED_LVL_5 = preload("res://CenteredLevels/centered_lvl_5.tscn")
-const CENTERED_LVL_6 = preload("res://CenteredLevels/centered_lvl_6.tscn")
-const CENTERED_LVL_7 = preload("res://CenteredLevels/centered_lvl_7.tscn")
 
 var levels_list = [\
-	CENTERED_LVL_1,\
-	CENTERED_LVL_2,\
-	CENTERED_LVL_3,\
-	CENTERED_LVL_4,\
-	CENTERED_LVL_5,\
-	CENTERED_LVL_6,\
-	CENTERED_LVL_7]
+	preload("res://Levels/Lvl1.tscn"),\
+	preload("res://Levels/Lvl2.tscn"),\
+	preload("res://Levels/Lvl3.tscn"),\
+	preload("res://Levels/level_forest.tscn"),\
+	preload("res://Levels/LvlPelouse.tscn"),\
+	preload("res://Levels/LvlParking.tscn"),\
+	preload("res://Levels/LvlAutoroute.tscn")]
 
 var current_scene_index = 0
+
+@onready var current_scene = $Game/Lvl1
+@onready var controls: ControlPanel = %Controls
+@onready var game: CanvasLayer = %Game
 
 func _ready() -> void:
 	Events.next_level.connect(on_next_level)
@@ -36,10 +30,6 @@ func _deferred_goto_scene(scene):
 	current_scene.free()
 	current_scene = scene.instantiate()
 	# Add it to the active scene, as child of root.
-	#get_tree().root.add_child(current_scene)
-	add_child(current_scene)
-	match current_scene_index:
-		2:
-			current_scene.position = Vector2(382.0, 314.0)
+	game.add_child(current_scene)
 	# Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
 	#get_tree().current_scene = current_scene
