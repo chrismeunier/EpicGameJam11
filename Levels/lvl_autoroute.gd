dc203f78cc89c0c891686904761eb53c44fee5b4
Hugo <Hugo.serdouragoncalves@gmail.com>
2025-06-29 12:56:25 +0200

    Fix stef levels

---
+++ Levels/lvl_autoroute.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var tilemap = $Map
+
+func _ready() -> void:
+	$Player.set_tilemap(tilemap)
+	$Grid.set_tilemap(tilemap)
+	$Vroum.set_path(get_car_path_for_world1())
+	$Vroum.set_tilemap(tilemap)
+	$Vroum2.set_path(get_car_path_for_world2())
+	$Vroum2.set_tilemap(tilemap)
+	$Vroum3.set_path(get_car_path_for_world1())
+	$Vroum3.set_tilemap(tilemap)
+
+func get_car_path_for_world1() -> Array:
+	var path := []
+	for i in 19:
+		path.append(Vector2.LEFT)
+
+	for i in 19:
+		path.append(Vector2.RIGHT)
+		
+	return path
+	
+func get_car_path_for_world2() -> Array:
+	var path := []
+	for i in 19:
+		path.append(Vector2.RIGHT)
+
+	for i in 19:
+		path.append(Vector2.LEFT)
+		
+	return path
