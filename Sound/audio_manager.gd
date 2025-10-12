fedc053d274a38dbf243d2f94f273a6a12827aac
Christophe MULLER <christophe.muller@espi-suisse.ch>
2025-10-12 17:24:59 +0200

    Force the polyphony for all AudioStreamPlayers in manager

--- Sound/audio_manager.gd
+++ Sound/audio_manager.gd
@@ -24,4 +24,12 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for child in get_children():
+		_set_polyphony(child)
+	
+func _set_polyphony(node: Node):
+	if node is AudioStreamPlayer:
+		node.set_max_polyphony(20)
+	elif node is Node:
+		for child in node.get_children():
+			_set_polyphony(child)
