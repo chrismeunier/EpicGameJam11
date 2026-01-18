3cb269fc423cabb85435651a4b10d31fc06bbf6c
Hugo <Hugo.serdouragoncalves@gmail.com>
2026-01-18 17:24:35 +0100

    Add save system

--- main.gd
+++ main.gd
@@ -11,6 +11,8 @@ const LVL_6 = preload("res://Levels/Lvl6.tscn")
 const LVL_7 = preload("res://Levels/Lvl7.tscn")
 const LVL_8 = preload("res://Levels/Lvl8.tscn")
 
+var config = ConfigFile.new()
+
 var levels_list = [\
 	LVL_1,\
 	LVL_2,\
@@ -21,19 +23,37 @@ var levels_list = [\
 	LVL_7,\
 	LVL_8]
 
-var current_scene_index = 0
+var current_scene_index = load_progression_and_get_current_index()
 
 func _ready() -> void:
 	Events.next_level.connect(on_next_level)
 	Events.reset_level.connect(on_reset_level)
+	load_lvl()
 
 func on_next_level() -> void:
 	current_scene_index += 1
+	save_progression(current_scene_index)
 	var amountLevel = levels_list.size()
 	if current_scene_index >= amountLevel - 1:
 		current_scene_index = amountLevel - 1
+	load_lvl()
+
+func load_lvl() -> void:
 	var scene = levels_list[current_scene_index]
 	call_deferred("_deferred_goto_scene", scene)
+
+func save_progression(current_scene_index) -> void:
+	config.set_value("Player1", "current_index_level", current_scene_index)
+	config.save("user://progression.cfg")
+	
+func load_progression_and_get_current_index() -> int:
+	# Load data from a file.
+	var err = config.load("user://progression.cfg")
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return 0
+	
+	return config.get_value("Player1", "current_index_level")
 
 func _deferred_goto_scene(scene):
 	for child in self.get_children():
